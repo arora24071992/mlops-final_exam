f260b1a66fc6df41cd4b9bb0fcfdaf07d91fe60f
arora24071992 <sahilarora@Sahils-MacBook-Pro.local>
2022-11-20 21:37:40 +0530

    Final Exam

--- api/app.py
+++ api/app.py
@@ -25,9 +25,16 @@ def sum():
 
 @app.route("/predict", methods=['POST'])
 def predict_digit():
-    image = request.json['image']
+    image = request.json['image1']
     model_name = request.json['model_name']
-    model = load(model_name)
+    if model_name == None:
+         model = load(glob.glob(f"./models/"+"{model_name}"+"_*.joblib")[0])
+    else:
+        model = load(model_name)
+
     print("Model loaded")
     predicted = model.predict([image])
     return {"y_predicted":int(predicted[0])}
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", port=5000)
